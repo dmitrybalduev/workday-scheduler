bd82f54be8701838795ebfcd3702b3dff9693224
Dmitry <dmitry.balduev@gmail.com>
2021-03-16 22:50:28 -0400

    Added time block creation code

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 $("#currentDay").text( moment().format("dddd, MMMM Do YYYY"));
 
+function loadTimeblocks(){
+    //create block for cheduler
+    let timeBlock = $("<div></div>");
+    timeBlock.addClass('d-flex bd-highlight');
+    //create a left element displaying time
+    let displayTime = $("<div></div>").text("9am"); 
+    displayTime.addClass('p-2 bd-highlight');
+    //create a middle element for event name
+    let displayEvent = $("<div></div>").text("Some Event");
+    displayEvent.addClass('p-2 w-100 bd-highlight'); 
+    //create a right element for save button
+    let displaySaveButton = $("<div></div>").text("SAVE"); 
+    displaySaveButton.addClass('p-2 bd-highlight');
+    //append these elements
+    timeBlock.append(displayTime, displayEvent, displaySaveButton);
+    $('.container').append(timeBlock);
+}
 
-
-
-
+loadTimeblocks();
 
 
 
