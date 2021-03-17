b50d8f834ef57cd66be390222a68cf8df5d4be86
Dmitry <dmitry.balduev@gmail.com>
2021-03-16 23:52:44 -0400

    Added time array

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
+let timeArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm"]
+
+
 $("#currentDay").text( moment().format("dddd, MMMM Do YYYY"));
 
 function loadTimeblocks(){
     for(let i = 0; i < 12; i++){
         //create block for cheduler
-    let timeBlock = $("<div></div>");
-    timeBlock.addClass('d-flex bd-highlight');
-    //create a left element displaying time
-    let displayTime = $("<div></div>").text("9am"); 
-    displayTime.addClass('p-2 bd-highlight');
-    //create a middle element for event name
-    let displayEvent = $("<div></div>").text("Some Event");
-    displayEvent.addClass('p-2 w-100 bd-highlight'); 
-    //create a right element for save button
-    let displaySaveButton = $("<div></div>").text("SAVE"); 
-    displaySaveButton.addClass('p-2 bd-highlight');
-    //append these elements
-    timeBlock.append(displayTime, displayEvent, displaySaveButton);
-    $('.container').append(timeBlock);
+        let timeBlock = $("<div></div>");
+        timeBlock.addClass('d-flex bd-highlight');
+        //create a left element displaying time
+        let displayTime = $("<div></div>").text(timeArray[i]); 
+        displayTime.addClass('p-2 bd-highlight').attr("data-index", i);
+        //create a middle element for event name
+        let displayEvent = $("<div></div>").text("Some event");
+        displayEvent.addClass('p-2 w-100 bd-highlight').attr("data-index", i); 
+        //create a right element for save button
+        let displaySaveButton = $("<div></div>").text("SAVE"); 
+        displaySaveButton.addClass('p-2 bd-highlight').attr("data-index", i);
+        //append these elements
+        timeBlock.append(displayTime, displayEvent, displaySaveButton);
+        $('.container').append(timeBlock);
     }
-    
 }
 
 loadTimeblocks();
-
-
 
