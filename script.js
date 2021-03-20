b4025546cc343e9f3795968c9d477e7a9a5335ca
Dmitry <dmitry.balduev@gmail.com>
2021-03-20 15:23:09 -0400

    added colors

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-let timeArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm"]
+let timeArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+let timeArray2 = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var now = new Date().getHours();
+console.log(now);
+let color = "";
 
 
 $("#currentDay").text( moment().format("dddd, MMMM Do YYYY"));
 
 function loadTimeblocks(){
-    for(let i = 0; i < 12; i++){
+    for(let i = 0; i < timeArray.length; i++){
+        if(now > timeArray2[i]){
+            color = "bg-secondary";
+        } else if(now == timeArray2[i]){
+            color = "bg-danger";
+        }else{
+            color = "bg-success";
+        }
         //create block for cheduler
         let timeBlock = $("<div></div>");
-        timeBlock.addClass('d-flex bd-highlight');
+        timeBlock.addClass("d-flex bd-highlight " + color);
         //create a left element displaying time
         let displayTime = $("<div></div>").text(timeArray[i]); 
         displayTime.addClass('p-2 bd-highlight').attr("data-index", i);
